275afd638242d246e16cfbed43f34e506ef488a3
JerryS <jerrys@JerrySs-MacBook-Pro.local>
2020-11-30 09:20:16 +0800

    change summary_table into a function, center pie chart and bar plot, format chart descriptions

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -1,22 +1,29 @@
 library("dplyr")
 
+# Read the data file
 grad_stu_df <- read.csv("data/all-ages.csv",
   stringsAsFactors = FALSE
 )
 
-summary_table <- grad_stu_df %>%
-  group_by(Major_category) %>%
-  summarise(
-    avg_median = round(mean(Median, na.rm = TRUE), 0),
-    avg_p25th = round(mean(P25th, na.rm = TRUE), 0),
-    avg_p75th = round(mean(P75th, na.rm = TRUE), 0)
-  ) %>%
-  arrange(-avg_median)
-
-displayed_table <- summary_table %>%
-  rename(
-    "Major Category" = Major_category,
-    "Average Median Salary" = avg_median,
-    "Average 25th Percentile Salary" = avg_p25th,
-    "Average 75th Percentile Salary" = avg_p75th
+# create a function that reads a dataframe as an argument, and return the 
+# summary values of that dataframe. The return includes "Major Category" 
+# "Average Median Salary" "Average 25th Percentile Salary" "Average 75th
+# Percentile Salary".
+get_summary_table <- function(df){
+  return(
+    df %>%
+      group_by(Major_category) %>%
+      summarise(
+        avg_median = round(mean(Median, na.rm = TRUE), 0),
+        avg_p25th = round(mean(P25th, na.rm = TRUE), 0),
+        avg_p75th = round(mean(P75th, na.rm = TRUE), 0)
+      ) %>%
+      arrange(-avg_median) %>%
+    rename(
+      "Major Category" = Major_category,
+      "Average Median Salary" = avg_median,
+      "Average 25th Percentile Salary" = avg_p25th,
+      "Average 75th Percentile Salary" = avg_p75th
+    )
   )
+}
