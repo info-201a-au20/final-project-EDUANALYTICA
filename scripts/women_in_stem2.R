e7702d7384294ce7df992d19dfd11b7e8e30b43f
Ian Wang <35878784+iwangy@users.noreply.github.com>
2020-11-30 16:43:21 -0800

    figuring out women in stem pie chart

--- scripts/women_in_stem2.R
+++ scripts/women_in_stem2.R
@@ -4,20 +4,23 @@ library(plotly)
 library(lintr)
 library(stringr)
 
-women_stem <- read.csv("data/women-stem.csv", stringsAsFactors = FALSE)
+women_stem <- read.csv("../data/women-stem.csv", stringsAsFactors = FALSE)
 
 women_by_majors <- women_stem %>% 
   select(Major_category, Total, Men, Women, ShareWomen) %>% 
   group_by(Major_category) %>% 
   summarise(
-    TotalShareMen = sum(Men, na.rm = TRUE), # / sum(Total, na.rm = TRUE),
-    TotalShareWomen = sum(Women, na.rm = TRUE), # / sum(Total, na.rm = TRUE),
-    TotalGenders = TotalShareMen + TotalShareWomen
+    TotalShareMen = sum(Men, na.rm = TRUE)/sum(Total, na.rm = TRUE), # / sum(Total, na.rm = TRUE),
+    TotalShareWomen = sum(Women, na.rm = TRUE)/sum(Total, na.rm = TRUE), # / sum(Total, na.rm = TRUE),
   )
+women_by_majors_transpose <- data.frame(t(women_by_majors))
 
-gender <- c("Women", "Men")
 
-pie_menvswomen <- ggplot(women_by_majors, 
-                         mapping = aes(
-                           x = factor(1), y = "", fill = factor(TotalShareWomen))) + 
-  geom_bar(width = 1, stat = "identity", color = "white")
+pie_menvswomen <- ggplot(women_by_majors_transpose)+
+  geom_bar(mapping = aes(x = TotalShareMen, fill = Major_category))+
+  coord_polar(theta = "x")+
+  facet_wrap(~Major_category)
+
+                      
+                      
+          
