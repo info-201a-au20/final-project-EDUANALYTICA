5854a37ef0eb424b0a3aa7958c9b64be478119a3
Ian Wang <35878784+iwangy@users.noreply.github.com>
2020-11-30 19:55:35 -0800

    improvements to women_in_stem2

--- scripts/women_in_stem2.R
+++ scripts/women_in_stem2.R
@@ -8,19 +8,25 @@ women_stem <- read.csv("../data/women-stem.csv", stringsAsFactors = FALSE)
 
 women_by_majors <- women_stem %>% 
   select(Major_category, Total, Men, Women, ShareWomen) %>% 
-  group_by(Major_category) %>% 
+  group_by(Major_category) %>%
   summarise(
-    TotalShareMen = sum(Men, na.rm = TRUE)/sum(Total, na.rm = TRUE), # / sum(Total, na.rm = TRUE),
-    TotalShareWomen = sum(Women, na.rm = TRUE)/sum(Total, na.rm = TRUE), # / sum(Total, na.rm = TRUE),
+    Men = sum(Men, na.rm = TRUE)/sum(Total, na.rm = TRUE), # / sum(Total, na.rm = TRUE),
+    Women = sum(Women, na.rm = TRUE)/sum(Total, na.rm = TRUE), # / sum(Total, na.rm = TRUE),
   )
-women_by_majors_transpose <- data.frame(t(women_by_majors))
+
+women_by_majors_test <- data.frame(t(women_by_majors)) %>% 
+  rename("X1" = "Biology & Life Sciences", "X2" = "Computers & Mathematics", "X3" = "Engineering", "X4" = "Health", "X5" = "Physical Sciences")
 
 
-pie_menvswomen <- ggplot(women_by_majors_transpose)+
-  geom_bar(mapping = aes(x = TotalShareMen, fill = Major_category))+
-  coord_polar(theta = "x")+
+
+  mutate(Gender = list("Male" = "Male", "Female" = "Female"))
+
+
+
+pie_menvswomen <- ggplot(data = women_by_majors)+
+  geom_bar(mapping = aes(x = TotalShareMen, y = TotalShareWomen))+
   facet_wrap(~Major_category)
-
+  
                       
                       
           
