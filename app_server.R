9fcaf7b4888dcd431195dddcd5f5e4bf5906c7c6
Ian Wang <35878784+iwangy@users.noreply.github.com>
2020-12-05 21:08:24 -0800

    app server framework set, widget naming conventions

--- app_server.R
+++ app_server.R
@@ -1,3 +1,47 @@
+# WIDGET NAMING CONVENTIONS CHARTTYPE_WIDGET_#
+
+# INTRO - BRENDA'S SECTION
+
+
+
+
+
+
+
+
+# WOMEN vs MEN pie chart - JERRY'S SECTION
+
+
+
+
+
+
+
+
+
+# RECENTGRAD vs GRAD line chart - LEON'S SECTION
+
+
+
+
+
+
+
+
+
+# MAJOR UNEMPLOYED vs EMPLOYED bar plot - NICOLE'S SECTION
+
+
+
+
+
+
+
+
+
+# CONCLUSION TAKEAWAYS - IAN'S SECTION
+
+
 my_server <- function(input, output){
   
 }
--- app_ui.R
+++ app_ui.R
@@ -1,3 +1,23 @@
+library("Shiny")
+
+
+intro_sidebar_content 
+intro_main_content
+
+pie_sidebar_content
+pie_main_content
+
+line_sidebar_content
+line_main_content
+
+bar_sidebar_content
+bar_main_content
+
+conc_sidebar_content
+conc_main_content
+
+
+
 my_ui <- navbarPage(
   
 )
