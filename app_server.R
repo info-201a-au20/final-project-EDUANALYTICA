fefc6fa989d757e3af7809b19242031254c705e2
Leon Kan <leonykan@uw.edu>
2020-12-06 20:55:18 -0800

    display static lineplot and slider range on Shiny app

--- app_server.R
+++ app_server.R
@@ -1,5 +1,4 @@
 # import libraries
-library(shiny)
 library(dplyr)
 library(ggplot2)
 library(plotly)
@@ -70,42 +69,34 @@ p("The authors are students at the University of Washington studying Informatics
 recent_grad <- read.csv("data/recent-grads.csv", stringsAsFactors = FALSE)
 grad_students <- read.csv("data/grad-students.csv", stringsAsFactors = FALSE)
 
-median_salary_recent_grad <- recent_grad %>% 
-  select(Major, Major_category, Median) %>% 
-  group_by(Major_category) %>% 
+median_salary_recent_grad <- recent_grad %>%
+  select(Major, Major_category, Median) %>%
+  group_by(Major_category) %>%
   summarise(
     medianRecent = mean(Median, na.rm = TRUE)
   )
-median_salary_grad_students <- grad_students %>% 
-  select(Major, Major_category, Grad_median) %>% 
-  group_by(Major_category) %>% 
+median_salary_grad_students <- grad_students %>%
+  select(Major, Major_category, Grad_median) %>%
+  group_by(Major_category) %>%
   summarise(
     medianGrad = mean(Grad_median, na.rm = TRUE)
   )
 
-line_plot_data <- left_join(median_salary_recent_grad, 
+line_plot_data <- left_join(median_salary_recent_grad,
                             median_salary_grad_students)
 
-line_plot <- plot_ly(line_plot_data, 
-                     x = ~Major_category, y = ~medianRecent, 
+LinePlot_widget <- plot_ly(line_plot_data,
+                     x = ~Major_category, y = ~medianRecent,
                      name = "Median Salary of Recent graduates",
                      type = "scatter", mode = 'lines',
                      line = list(color = 'rgb(205, 12, 24', width = 3))
-line_plot <- line_plot %>%  
+LinePlot_widget <- LinePlot_widget %>%
   add_trace(y = ~medianGrad, name = "Median Salary of Graduates",
             line = list(color = 'rgb(22, 96, 167)', width = 3, dash = 'dash'))
-line_plot <- line_plot %>% 
-  layout(title = " The Comparison of Median Salary between Recent grad and grad",
+LinePlot_widget <- LinePlot_widget %>%
+  layout(title = "The Comparison of Median Salary between Recent grad and grad",
          xaxis = list(title = "Major Categories"),
          yaxis = list(title = "Median Salaries in dollar"))
-line_plot
-
-# implement UI: widget and side/main panel
-# widget - sliding bar: y = salary median
-
-
-
-
 
 
 # MAJOR UNEMPLOYED vs EMPLOYED bar plot - NICOLE'S SECTION
@@ -121,6 +112,10 @@ line_plot
 # CONCLUSION TAKEAWAYS - IAN'S SECTION
 
 
+# Server function
 my_server <- function(input, output){
-  intro_widget
+  
+  output$LinePlot_Range <- renderPrint({ input$LinePlot_SliderBar })
+  
+  output$LinePlot_widget <- renderPlotly({ LinePlot_widget })
 }
--- app_ui.R
+++ app_ui.R
@@ -1,24 +1,47 @@
-library("Shiny")
+library("shiny")
 library("plotly")
 library("ggplot2")
 
-intro_sidebar_content 
-intro_main_content
+# intro_sidebar_content 
+# intro_main_content
+# 
+# pie_sidebar_content
+# pie_main_content
 
-pie_sidebar_content
-pie_main_content
+line_sidebar_content <- sidebarPanel(
+  h2("Select a range of median salary"),
+  fluidPage(
+    fluidRow(
+      column(8,
+             sliderInput(inputId = "LinePlot_SliderBar", label = h3("Slider Range"), min = 30, 
+                         max = 100, value = c(50, 70))
+      )
+    ),
+    
+    hr(),
+    
+    fluidRow(
+      column(8, verbatimTextOutput("LinePlot_Range"))
+    )
+  )
+)
 
-line_sidebar_content
-line_main_content
+line_main_content <- mainPanel(
+  h1("The Comparison of Median Salary between Recent grad and grad"),
+  plotlyOutput(
+    outputId = "LinePlot_widget"
+  )
+)
 
-bar_sidebar_content
-bar_main_content
-
-conc_sidebar_content
-conc_main_content
+# bar_sidebar_content
+# bar_main_content
+# 
+# conc_sidebar_content
+# conc_main_content
 
 
 
 my_ui <- navbarPage(
-  
+  line_sidebar_content,
+  line_main_content
 )
