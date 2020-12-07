7af4dd5db29d3b47a6dd85153b7e01bea426fab7
JerryS <zsun0510@uw.edu>
2020-12-07 15:59:53 +0800

    finish pie chart version one

--- app_server.R
+++ app_server.R
@@ -1,47 +1,94 @@
-# WIDGET NAMING CONVENTIONS CHARTTYPE_WIDGET_#
+library("shiny")
+library("dplyr")
+library("tidyr")
+library("ggplot2")
+library("plotly")
+library("knitr")
 
-# INTRO - BRENDA'S SECTION
-
-
-
-
-
-
-
-
-# WOMEN vs MEN pie chart - JERRY'S SECTION
-
-
-
-
-
-
-
-
-
-# RECENTGRAD vs GRAD line chart - LEON'S SECTION
-
-
-
-
-
-
-
-
-
-# MAJOR UNEMPLOYED vs EMPLOYED bar plot - NICOLE'S SECTION
-
-
-
-
-
-
-
-
-
-# CONCLUSION TAKEAWAYS - IAN'S SECTION
-
-
-my_server <- function(input, output){
-  
-}
+# my_server <- function(input, output){
+#   
+# # WIDGET NAMING CONVENTIONS CHARTTYPE_WIDGET_#
+# 
+# # INTRO - BRENDA'S SECTION
+# 
+# 
+# 
+# 
+#   
+# 
+# 
+# 
+# # WOMEN vs MEN pie chart - JERRY'S SECTION
+#   num_men <- read.csv("data/women-stem.csv", 
+#                       stringsAsFactors = FALSE) %>% 
+#     filter(Major_category == input$pie_widget_one) %>% 
+#     summarize(
+#       sum_men = sum(Men, na.rm = TRUE),
+#       sum_women = sum(Women, na.rm = TRUE)
+#     ) %>% 
+#     pull(
+#       sum_men
+#     )
+#   num_women <- read.csv("data/women-stem.csv", 
+#                         stringsAsFactors = FALSE) %>% 
+#     filter(Major_category == input$pie_widget_one) %>% 
+#     summarize(
+#       sum_women = sum(Women, na.rm = TRUE)
+#     ) %>% 
+#     pull(
+#       sum_women
+#     )
+#   
+#   gender <- c("Men", "Women")
+#   num <- c(num_men, num_women)
+#   df <- data.frame(gender, num)
+#   output$pieplot <- renderPlot({
+#       pie_plot <- pie(num, labels = gender)
+#       return(pie_plot)
+#       # plot_ly(df, labels = ~gender, values = ~num, type = 'pie', textposition = 'inside',
+#       #       textinfo = 'label+percent',
+#       #       insidetextfont = list(color = '#FFFFFF'), hoverinfo = 'text',
+#       #       text = ~paste0(gender, ": ", num),  
+#       #       marker = list(colors = colors,line = list(color = '#FFFFFF', 
+#       #                                                 width = 1)),
+#       #       showlegend = FALSE) # %>%
+#       # layout(autosize = F, width = 500, height = 500,
+#       #        title = ~paste0("Percentage of Men and Women in ", 
+#       #                        input$pie_widget_one, " <br /> (Total in Major: ", 
+#       #                        num_men + num_women, " )"),  
+#       #        showlegend = F,
+#       #        xaxis = list(showgrid = FALSE, zeroline = FALSE, 
+#       #                     showticklabels = FALSE),
+#       #        yaxis = list(showgrid = FALSE, zeroline = FALSE, 
+#       #                     showticklabels = FALSE))
+#   })
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# # RECENTGRAD vs GRAD line chart - LEON'S SECTION
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# # MAJOR UNEMPLOYED vs EMPLOYED bar plot - NICOLE'S SECTION
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# # CONCLUSION TAKEAWAYS - IAN'S SECTION
+# }
--- app_ui.R
+++ app_ui.R
@@ -1,23 +1,39 @@
-library("Shiny")
+library("shiny")
+library("dplyr")
 
+women_in_stem <- read.csv("data/women-stem.csv", stringsAsFactors = FALSE)
+major_category <- women_in_stem %>% 
+  group_by(Major_category) %>% 
+  summarize() %>% 
+  pull(Major_category)
 
-intro_sidebar_content 
-intro_main_content
+# intro_sidebar_content 
+# intro_main_content
 
-pie_sidebar_content
-pie_main_content
+pie_sidebar_content <- sidebarPanel(
+  selectInput(
+    inputId = "pie_widget_one",
+    label = "Major Category:",
+    choice = major_category
+  )
+)
+pie_main_content <- mainPanel(
+  plotOutput("pieplot")
+)
 
-line_sidebar_content
-line_main_content
-
-bar_sidebar_content
-bar_main_content
-
-conc_sidebar_content
-conc_main_content
-
-
+# line_sidebar_content
+# line_main_content
+# 
+# bar_sidebar_content
+# bar_main_content
+# 
+# conc_sidebar_content
+# conc_main_content
+# 
+# 
 
 my_ui <- navbarPage(
-  
+  tabPanel(
+    pie_sidebar_content
+  )
 )
