cbf8c3bb1a0cce3de4735d72760a6e562677e56e
JerryS <zsun0510@uw.edu>
2020-12-08 10:50:57 +0800

    debug pie chart

--- app_ui.R
+++ app_ui.R
@@ -10,30 +10,30 @@ library("ggplot2")
 # pie_main_content
 
 # import combined dataframe from server.r
-source("app_server.R")
-line_plot_data
-
-salary_range_recent <- range(line_plot_data$salary_index)
-
-line_sidebar_content <- sidebarPanel(
-  h2("Select a range of median salary"),
-  # radioButtons("user_select", label = h3("Radio buttons"),
-  #              choices = list("Median of Recent Graduates" = 1, 
-  #                             "Average of Medians" = 2, 
-  #                             "Median of Graduates" = 3), 
-  #              selected = avg),
-  sliderInput(inputId = "LinePlot_SliderBar", label = h3("Slider Range"), 
-              min = salary_range_recent[1], max = salary_range_recent[2], 
-              value = salary_range_recent),
-)
-
-
-line_main_content <- mainPanel(
-  h1("The Comparison of Median Salary between Recent grad and grad"),
-  plotlyOutput(
-    outputId = "LinePlot_widget"
-  )
-)
+# source("app_server.R")
+# line_plot_data
+# 
+# salary_range_recent <- range(line_plot_data$salary_index)
+# 
+# line_sidebar_content <- sidebarPanel(
+#   h2("Select a range of median salary"),
+#   # radioButtons("user_select", label = h3("Radio buttons"),
+#   #              choices = list("Median of Recent Graduates" = 1, 
+#   #                             "Average of Medians" = 2, 
+#   #                             "Median of Graduates" = 3), 
+#   #              selected = avg),
+#   sliderInput(inputId = "LinePlot_SliderBar", label = h3("Slider Range"), 
+#               min = salary_range_recent[1], max = salary_range_recent[2], 
+#               value = salary_range_recent),
+# )
+# 
+# 
+# line_main_content <- mainPanel(
+#   h1("The Comparison of Median Salary between Recent grad and grad"),
+#   plotlyOutput(
+#     outputId = "LinePlot_widget"
+#   )
+# )
 
 women_in_stem <- read.csv("data/women-stem.csv", stringsAsFactors = FALSE)
 major_category <- women_in_stem %>% 
@@ -52,16 +52,16 @@ pie_sidebar_content <- sidebarPanel(
   )
 )
 pie_main_content <- mainPanel(
-  plotOutput("pieplot")
+  plotlyOutput("pieplot")
 )
 
-page_three <- tabPanel(
-  "Page 3",
-  sidebarLayout(
-    line_sidebar_content,
-    line_main_content
-  )
-)
+# page_three <- tabPanel(
+#   "Page 3",
+#   sidebarLayout(
+#     line_sidebar_content,
+#     line_main_content
+#   )
+# )
 
 # bar_sidebar_content
 # bar_main_content
@@ -72,6 +72,7 @@ page_three <- tabPanel(
 # 
 
 
-my_ui <- navbarPage(
-  page_three
+my_ui <- fluidPage(
+  pie_sidebar_content,
+  pie_main_content
 )
